d536706622c099c4e60fa82d80dfb08bf096cd43
Satya J <Satyanarayana.J@cognizant.com>
2016-04-13 16:44:37 +0530

    Added DnD functionality

--- public/scripts/app/app.js
+++ public/scripts/app/app.js
@@ -6,7 +6,7 @@
 * Description
 */
 
-angular.module('canvas', ['ngRoute'])
+angular.module("canvas", ["ngRoute","ngDragDrop"])
 
 .config(function($routeProvider) {
 	$routeProvider.otherwise("/");
@@ -15,5 +15,165 @@ angular.module('canvas', ['ngRoute'])
 		controller:"CanvasController"
 	});
 	console.log("configured");
+})
+.directive('draggable', function(){
+  // Runs during compile
+  return function(scope,element){
+    var el = element[0];
+
+        el.draggable = true;
+
+        el.addEventListener(
+            'dragstart',
+            function(e) {
+                e.dataTransfer.effectAllowed = 'move';
+                e.dataTransfer.setData('Text', this.id);
+                this.classList.add('drag');
+                return false;
+            },
+            false
+        );
+
+        el.addEventListener(
+            'dragend',
+            function(e) {
+                this.classList.remove('drag');
+                return false;
+            },
+            false
+        );
+  };
+})
+.directive('droppable', function() {
+    return {
+        scope: {
+          drop:'&'
+        },
+        link: function(scope, element) {
+            // again we need the native object
+          var el = element[0];
+          el.addEventListener(
+          'dragover',
+          function(e) {
+            e.dataTransfer.dropEffect = 'move';
+            // allows us to drop
+            if (e.preventDefault) e.preventDefault();
+            this.classList.add('over');
+            return false;
+            },
+            false
+          );
+
+          el.addEventListener(
+    'dragenter',
+    function(e) {
+        this.classList.add('over');
+        return false;
+    },
+    false
+);
+
+el.addEventListener(
+    'dragleave',
+    function(e) {
+        this.classList.remove('over');
+        return false;
+    },
+    false
+);
+
+el.addEventListener(
+    'drop',
+    function(e) {
+        // Stops some browsers from redirecting.
+        if (e.stopPropagation) e.stopPropagation();
+
+        this.classList.remove('over');
+
+        var item = document.getElementById(e.dataTransfer.getData('Text'));
+        if(item.parentNode && item.parentNode.id!='dropper') {
+          var item_clone = item.cloneNode(true);
+          item_clone.classList.add('draggable');
+          this.appendChild(item_clone);
+        }
+        console.log(e);
+        //this.appendChild(item);
+        scope.$apply(function(scope){
+          var fn = scope.drop();
+          if ('undefined' !== typeof fn) {
+            fn(item.id, binId);
+          }
+        });
+        return false;
+    },
+    false
+);
+
+
+        }
+    }
+})
+.directive("drawing", function(){
+  return {
+    restrict: "A",
+    link: function(scope, element){
+      var ctx = element[0].getContext('2d');
+      
+      // variable that decides if something should be drawn on mousemove
+      var drawing = false;
+      
+      // the last coordinates before the current move
+      var centerX;
+      var centerY;
+      var currentX;
+      var currentY;
+      
+      element.bind('mousedown', function(event){
+        
+        centerX = event.offsetX;
+        centerY = event.offsetY;
+        
+        // begins new line
+        ctx.beginPath();
+        
+        drawing = true;
+      });
+
+      element.bind('mousemove', function(event){
+        if(drawing){
+          // get current mouse position
+          //console.log(event);
+          currentX = event.offsetX;
+          currentY = event.offsetY;
+          
+          draw(centerX, centerY, currentX, currentY);
+        }
+        
+      });
+
+      element.bind('mouseup', function(event){
+        // stop drawing
+        drawing = false;
+      });
+        
+      // canvas reset
+      function reset(){
+       element[0].width = element[0].width; 
+      }
+      
+      function draw(startX, startY, currentX, currentY){
+        reset();
+        var sizeX = currentX - startX;
+        var sizeY = currentY - startY;
+        
+        ctx.rect(startX, startY, sizeX, sizeY);
+        ctx.lineWidth = 3;
+        // color
+        ctx.strokeStyle = '#fff';
+        // draw it
+        ctx.stroke();
+      }
+    }
+  };
 });
 
--- public/scripts/app/home/home.controller.js
+++ public/scripts/app/home/home.controller.js
@@ -5,11 +5,9 @@
 *
 * Description
 */
-angular.module('canvas')
+angular.module("canvas")
 
-.controller('CanvasController',function($scope){
-	var canv = $("canv");
-	console.log(canv);
+.controller("CanvasController",function($scope){
 	//var ctx = canv.getContext("2d");
 	//ctx.rect(20, 20, 150, 100);
 	//ctx.stroke();
